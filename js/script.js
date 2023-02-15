6b995d532072a21ee2209a5d1c3fe7b8d40443e9
Weverton <wever-santos@hotmail.com>
2023-02-15 17:50:35 -0300

    captured the datas and create validations

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log(document.getElementById("novoItem"))
+const form = document.getElementById("novoItem")
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault()
+	const nome = e.target.elements['nome'].value
+	const quantidade = e.target.elements['quantidade'].value
+
+	criaElemento(nome, quantidade)
+})
+
+function criaElemento(nome, quantidade){
+	console.log(nome)
+	console.log(quantidade)
+
+	const novoItem = document.createElement('li')
+	novoItem.classList.add("item")
+
+	const numeroItem = document.createElement('strong')
+	numeroItem.innerHTML = quantidade
+
+	console.log(numeroItem)
+}
