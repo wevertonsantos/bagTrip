a617ce9e8fb2fe08f39ec8b1cf5c6a56f319d84f
Weverton <wever-santos@hotmail.com>
2023-02-16 14:47:39 -0300

    create the validation

--- js/main.js
+++ js/main.js
@@ -2,10 +2,23 @@ const form = document.getElementById("novoItem")
 
 form.addEventListener("submit", (e) => {
 	e.preventDefault()
-
 	criaElemento(e.target.elements['nome'].value, e.target.elements['quantidade'].value)
 })
 
 function criaElemento(nome, quantidade){
-	
+	console.log(nome)
+	console.log(quantidade)
+
+ 	const novoItem = document.createElement('li')
+ 	novoItem.classList.add('item')
+
+ 	const numeroItem = document.createElement('strong')
+ 	numeroItem.innerHTML = quantidade
+
+ 	const lista = document.getElementById('lista')
+
+ 	novoItem.appendChild(numeroItem)
+ 	novoItem.innerHTML += nome
+
+ 	lista.appendChild(novoItem)
 }
