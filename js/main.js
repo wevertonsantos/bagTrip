77072bd13ece6001bb22a20d81935776300a3008
Weverton <wever-santos@hotmail.com>
2023-02-16 15:21:11 -0300

    LocalStorage.setItem

--- js/main.js
+++ js/main.js
@@ -1,24 +1,30 @@
 const form = document.getElementById("novoItem")
+const lista = document.getElementById('lista')
 
 form.addEventListener("submit", (e) => {
 	e.preventDefault()
-	criaElemento(e.target.elements['nome'].value, e.target.elements['quantidade'].value)
+
+	const nome = e.target.elements['nome'].value
+	const quantidade = e.target.elements['quantidade'].value
+
+	criaElemento(nome, quantidade)
+
+	nome.value = ""
+	quantidade.value = ""
 })
 
 function criaElemento(nome, quantidade){
-	console.log(nome)
-	console.log(quantidade)
-
  	const novoItem = document.createElement('li')
  	novoItem.classList.add('item')
 
  	const numeroItem = document.createElement('strong')
  	numeroItem.innerHTML = quantidade
 
- 	const lista = document.getElementById('lista')
-
  	novoItem.appendChild(numeroItem)
  	novoItem.innerHTML += nome
 
  	lista.appendChild(novoItem)
+
+ 	LocalStorage.setItem('nome', nome)
+ 	LocalStorage.setItem('quantidade', quantidade)
 }
